df9e972e6ec812766f1ce4923d3a78cfa6b05a7d
Dardel <stevebeteta@gmail.com>
2018-04-19 16:14:05 -0600

    Bases para comenzar edición

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var mensajeEscrito = "Hola Mundo"
+  res.render('index', { title: 'Express', mensaje: mensajeEscrito });
 });
 
 module.exports = router;
