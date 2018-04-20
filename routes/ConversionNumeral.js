7898f003dada02df11f81dd8887c1448aa78d552
Dardel <stevebeteta@gmail.com>
2018-04-19 19:11:18 -0600

    Primera edición de conversiones

--- routes/ConversionNumeral.js
+++ routes/ConversionNumeral.js
@@ -2,9 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/ConversionNumeral', function(req, res, next) {
+router.get('/', function(req, res, next) {
   var mensajeEscrito = "Aqui debe estar la Conversión Numeral"
   res.render('ConversionNumeral', { title: 'Express', mensaje: mensajeEscrito });
 });
+
+router.get('/dec', function(req, res, next){
+  res.send("Hola mundo");
+})
 
 module.exports = router;
