ba3485be0b6e1642692c592f012d4e14681e15a3
Dardel <stevebeteta@gmail.com>
2018-04-20 18:45:33 -0600

    Conversores terminados con optimización para cualquier base numérica computacional

--- public/javascripts/conversores.js
+++ public/javascripts/conversores.js
@@ -1,55 +1,94 @@
-$(document).ready(function () {
-  // Aqui escribi el codigo de los botones de Botones Originales
-  var ValorOrigenTipo = "Decimal";
-  var valorConvertidoTipo = "Decimal";
+//Declaracionde variables Globales
+var valorOrigenTipo = "Decimal";
+var valorNumOrigenTipo = "10"
+var valorConvertidoTipo = "Decimal";
+var valorNumConvertidoTipo = "10"
+var valorEscrito = "Ingrese un Número Válido";
 
-  $("#OriDec").on("click",function () {
-    ValorOrigenTipo = "Decimal";
+$(document).ready(function() {
+
+  // Aqui escribi el codigo de los botones de Botones Originales
+  $("#resultado").text(valorEscrito);
+  $("#OriDec").on("click", function() {
+    valorOrigenTipo = "Decimal";
+    valorNumOrigenTipo = "10"
     $("#pregunta").val("");
-    $("#resultado").text("0");
-    $("#numeroOriginal").text(ValorOrigenTipo);
+    var valorEscrito = "Ingrese un Número Válido";
+    $("#resultado").text(valorEscrito);
+    $("#numeroOriginal").text(valorOrigenTipo);
   })
-  $("#OriBin").on("click",function () {
-    ValorOrigenTipo = "Binario";
+  $("#OriBin").on("click", function() {
+    valorOrigenTipo = "Binario";
+    valorNumOrigenTipo = "2"
     $("#pregunta").val("");
-    $("#resultado").text("0");
-    $("#numeroOriginal").text(ValorOrigenTipo);
+    var valorEscrito = "Ingrese un Número Válido";
+    $("#resultado").text(valorEscrito);
+    $("#numeroOriginal").text(valorOrigenTipo);
   })
-  $("#OriOct").on("click",function () {
-    ValorOrigenTipo = "Octal";
+  $("#OriOct").on("click", function() {
+    valorOrigenTipo = "Octal";
+    valorNumOrigenTipo = "8";
     $("#pregunta").val("");
-    $("#resultado").text("0");
-    $("#numeroOriginal").text(ValorOrigenTipo);
+    var valorEscrito = "Ingrese un Número Válido";
+    $("#resultado").text(valorEscrito);
+    $("#numeroOriginal").text(valorOrigenTipo);
   })
-  $("#OriHex").on("click",function () {
-    ValorOrigenTipo = "Hexagesimal";
+  $("#OriHex").on("click", function() {
+    valorOrigenTipo = "Hexagesimal";
+    valorNumOrigenTipo = "16";
     $("#pregunta").val("");
-    $("#resultado").text("0");
-    $("#numeroOriginal").text(ValorOrigenTipo);
+    var valorEscrito = "Ingrese un Número Válido";
+    $("#resultado").text(valorEscrito);
+    $("#numeroOriginal").text(valorOrigenTipo);
   })
+
   // Aqui escribi el codigo de los botones de Botones Convertidos
-  $("#ConDec").on("click",function (){
-    ValorOrigenTipo = "Decimal";
-    $("#numeroConvertido").text(ValorOrigenTipo)
+  $("#ConDec").on("click", function() {
+    valorConvertidoTipo = "Decimal";
+    valorNumConvertidoTipo = "10";
+    $("#numeroConvertido").text(valorConvertidoTipo);
+    var valor = $("#pregunta").val();
+    $("#resultado").text(converDardel(valorNumOrigenTipo,valorNumConvertidoTipo,valor));
   })
-  $("#ConBin").on("click",function (){
-    ValorOrigenTipo = "Binario";
-    $("#numeroConvertido").text(ValorOrigenTipo)
+  $("#ConBin").on("click", function() {
+    valorConvertidoTipo = "Binario";
+    valorNumConvertidoTipo = "2";
+    $("#numeroConvertido").text(valorConvertidoTipo);
+    var valor = $("#pregunta").val();
+    $("#resultado").text(converDardel(valorNumOrigenTipo,valorNumConvertidoTipo,valor));
   })
-  $("#ConOct").on("click",function (){
-    ValorOrigenTipo = "Octal";
-    $("#numeroConvertido").text(ValorOrigenTipo)
+  $("#ConOct").on("click", function() {
+    valorConvertidoTipo = "Octal";
+    valorNumConvertidoTipo = "8";
+    $("#numeroConvertido").text(valorConvertidoTipo);
+    var valor = $("#pregunta").val();
+    $("#resultado").text(converDardel(valorNumOrigenTipo,valorNumConvertidoTipo,valor));
   })
-  $("#ConHex").on("click",function (){
-    ValorOrigenTipo = "Hexagesimal";
-    $("#numeroConvertido").text(ValorOrigenTipo)
+  $("#ConHex").on("click", function() {
+    valorConvertidoTipo = "Hexagesimal";
+    valorNumConvertidoTipo = "16";
+    $("#numeroConvertido").text(valorConvertidoTipo);
+    var valor = $("#pregunta").val();
+    $("#resultado").text(converDardel(valorNumOrigenTipo,valorNumConvertidoTipo,valor));
   })
 
 
   //Luego dentro de cada boton hay que hacer el codigo para que cambie el textarea.
+
+  $("#pregunta").keyup(function() {
+    var valor = $("#pregunta").val();
+    $("#resultado").text(converDardel(valorNumOrigenTipo,valorNumConvertidoTipo,valor));
+
+  });
 });
 
-$("#pregunta").keyup(function(){
-  var valor = $("#pregunta").val();
-   $("#resultado").text(valor);
-});
+// Declarar la funcion y las variables
+function converDardel(sistemaOrigen, sistemaConvertir, valorFinal){
+  var valorConvertido = "0"
+  valorConvertido = parseInt(valorFinal, sistemaOrigen).toString(sistemaConvertir)
+  if (parseInt(valorConvertido, sistemaConvertir) >= 0 || parseInt(valorConvertido, sistemaOrigen) < 0 ) {
+      return valorConvertido;
+  } else {
+    return "Ya m cansé prro :'v ";
+  }
+}
